4c7d62beb7f2e496133da3f5ac33d7f8cf207ad5
GULSHAN KUMAR <gk178997@gmail.com>
2026-09-02 21:25:03 +0530

    Prepare backend for deployment

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,12 +1,16 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+const dns = require("dns");
+
+dns.setServers(["8.8.8.8", "1.1.1.1"]);
 
 const connectDB = async () => {
-    try {
-        await mongoose.connect(process.env.MONGO_URI);
-        console.log('MongoDB connected');
-    } catch (err) {
-        console.error(err);
-    }
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+
+    console.log("MongoDB connected");
+  } catch (err) {
+    console.error(err);
+  }
 };
 
 module.exports = connectDB;
